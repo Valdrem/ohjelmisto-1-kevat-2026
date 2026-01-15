f55adba7dc1c1530fb0ec5317e05cf612b1a9781
Valdrem <valtsu2003.dre@gmail.com>
2026-01-15 13:52:13 +0200

    commit 12-01-2026

--- Module 2/assignment5.py
+++ Module 2/assignment5.py
@@ -1,11 +1,11 @@
-from logging import logThreads
-from multiprocessing.util import log_to_stderr
+talents = float(input("Enter talents:"))
+pounds = float(input("Enter pounds:"))
+lots = float(input("Enter lots:"))
 
-talents = float(input("Enter talents: "))
-pounds = float(input("Enter pounds: "))
-lots = float(input("Enter lots: "))
+total_grams = (talents * 20 * 32 * 13.3) + (pounds * 32 * 13.3) + (lots * 13.3)
 
-gram_talents = talents * 20 + 32 + 13.4
-gram_lots = lots * 13.3
+total_kg_float = float(total_grams / 1000)
+total_kg = int(total_kg_float)
+total_grams = total_kg_float - total_kg
 
-print(f"The wigth in modern units: {gram_talents}")
+print(f"The weigth in modern units: {total_kg} kilograms and {total_grams} grams")
