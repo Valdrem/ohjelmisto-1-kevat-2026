d5f6a49b4b414f7ce6c921ea39b023e37dc9fd68
Valdrem <Valtsu2003.dre@gmail.com>
2026-01-15 12:39:43 +0200

    Delete Module 2/assingnment3.py

--- Module 2/assingnment3.py
+++
@@ -1,8 +0,0 @@
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
-
-perimeter = (length + width) * 2
-area = length * width
-
-print(f"The perimeter of the rectangle is {perimeter}")
-print(f"The area of the rectangle is {area}")
