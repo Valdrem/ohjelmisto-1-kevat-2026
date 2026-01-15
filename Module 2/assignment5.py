d756f0c0c76d1c37955280a1b151274655b46ccc
Valdrem <valtsu2003.dre@gmail.com>
2026-01-15 16:53:10 +0200

    commit 12-01-2026

--- Module 2/assignment5.py
+++ Module 2/assignment5.py
@@ -1,11 +1,11 @@
-talents = float(input("Enter talents:"))
-pounds = float(input("Enter pounds:"))
-lots = float(input("Enter lots:"))
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
 
 total_grams = (talents * 20 * 32 * 13.3) + (pounds * 32 * 13.3) + (lots * 13.3)
 
-total_kg_float = float(total_grams / 1000)
-total_kg = int(total_kg_float)
-total_grams = total_kg_float - total_kg
+kilograms = total_grams / 1000
+remaining_grams = total_grams % 1000
 
-print(f"The weigth in modern units: {total_kg} kilograms and {total_grams} grams")
+
+print(f"The weight in modern units:\n{int(kilograms)} kilograms and {remaining_grams:.2f} grams.")
