ead8b7d25fd43437a6277c78563f852002af3f1f
Valdrem <valtsu2003.dre@gmail.com>
2026-01-19 12:35:36 +0200

    commit 12-01-2026

--- Module 3/assignment3.py
+++ Module 3/assignment3.py
@@ -1,18 +1,21 @@
 gender = input("Enter biological gender (male/female): ")
-hemoglobing = int(input("Enter hemoglobin value (g/l): "))
+hemoglobin = float(input("Enter hemoglobin value (g/l): "))
 
-if gender == "male":
-    if hemoglobing >= 134 or hemoglobing >= 167:
+if gender.lower() == "male":
+    if hemoglobin >= 134 and hemoglobin <= 167:
         print("Your hemoglobin is normal.")
-    elif hemoglobing >= 167:
+    elif hemoglobin > 167:
         print("Your hemoglobin is high.")
     else:
         print("Your hemoglobin is low.")
 
-if gender == "female":
-    if hemoglobing >= 117 or hemoglobing >= 155:
+if gender.lower() == "female":
+    if hemoglobin >= 117 and hemoglobin <= 155:
         print("Your hemoglobin is normal.")
-    elif hemoglobing >= 155:
+    elif hemoglobin > 155:
         print("Your hemoglobin is high.")
     else:
         print("Your hemoglobin is low.")
+
+if gender.lower() != "male" and gender.lower() != "female":
+    print("Invalid gender.")
