30c46233e681bb3b53e918d83e5cab2d5d2f1cb6
Valdrem <valtsu2003.dre@gmail.com>
2026-01-19 09:16:02 +0200

    commit 12-01-2026

---
+++ sandbox.py
@@ -0,0 +1,17 @@
+var1 = "Hello"
+var2 = "Bye"
+
+if var1 == "Hello":
+    print(f"It's {var1}")
+else:
+    print(f"It's not {var1}")
+
+
+money = 5
+
+if money >= 5:
+    print("You have enough money")
+else:
+    print("You have not enough money")
+
+
