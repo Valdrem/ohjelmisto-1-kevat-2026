2945b7e6be2413c64121007fad3f87af26bb7b02
Valery D. <Valtsu2003.dre@gmail.com>
2026-01-26 20:07:01 +0200

    Delete Module4 directory

--- Module4/assignment2.py
+++
@@ -1,8 +0,0 @@
-inches = float(input("Enter length in inches (negative value to quit): "))
-
-while inches > -1:
-    centimeters = inches * 2.54
-    print(f"{inches} inches is {centimeters:2.2f} centimeters")
-    inches = float(input("Enter length in inches (negative value to quit): "))
-else:
-    print("Program ended.")
