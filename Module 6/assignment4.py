3826f7900172f9925be8f05a63110f0923df38ba
Valdrem <valtsu2003.dre@gmail.com>
2026-01-30 18:19:57 +0200

    commit 12-01-2026

--- Module 6/assignment4.py
+++ Module 6/assignment4.py
@@ -1,16 +1,10 @@
-integer = input("Enter a number: ")
-int_list = []
+number_list = [1, 2, 3, 4, 5]
 
-while integer != "":
-    integer = int(integer)
-    int_list.append(integer)
-    integer = input("Enter a number: ")
-
-def sum_of_list():
-    array_sum = sum(int_list)
+def sum_of_list(number_list):
+    array_sum = sum(number_list)
     return array_sum
 
-result = sum_of_list()
+result = sum_of_list(number_list)
 
 print(f"The sum of the numbers in the list is: {result}")
 
